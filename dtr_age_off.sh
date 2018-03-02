9c1ecf4110f159174e66dfd892489a19c7bd2a5a
Andy Clemenko <clemenko@gmail.com>
2018-03-02 14:27:45 -0800

    updates

--- dtr_age_off.sh
+++ dtr_age_off.sh
@@ -9,7 +9,6 @@ age=$2
 password=Pa22word
 dryrun=no
 
-BSD=true
 
 ######  NO MOAR EDITS #######
 RED=$(tput setaf 1)
@@ -25,7 +24,7 @@ date=
 image_list=$(curl -skX GET -u admin:$password "https://dtr.dockr.life/api/v0/repositories/?pageSize=10&count=false" -H "accept: application/json" |jq -r '.repositories[] | "\(.namespace)/\(.name)"')
 
 
-image_list="admin/alpine"
+#image_list="admin/alpine"
 for i in $image_list; do
   echo "-- $i --"
   tag_list=$(curl -skX GET -u admin:$password  "https://dtr.dockr.life/api/v0/repositories/$i/tags?pageSize=10&000count=false&includeManifests=false" -H "accept: application/json" | jq -r '.[].name')
@@ -33,10 +32,11 @@ for i in $image_list; do
     echo -n $x" "
     curl -skX GET -u admin:$password  "https://dtr.dockr.life/api/v0/repositories/$i/tags/$x?pageSize=10&000count=false&includeManifests=false" -H "accept: application/json" | jq -r '.[].createdAt' | awk -F":" '{print $1}' | awk -F"T" '{print $1}'
 
-    if [ "$BSD" = "true" ]; then
-# for date comparison on a mac
-    echo ""
-   fi
+    #date comparison
+
+    if [ "$dryrun" == "no" ]; then
+      echo "deleting all the things"
+    fi
 
   done
 done
